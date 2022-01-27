6dcd6d4672809c85cd2ce4ea8259740c5dfd857f
dylandoamaral <do.amaral.dylan@gmail.com>
2022-01-27 14:44:24 +0100

    fix: underlying dataset representation

--- src/main/scala/zio/spark/sql/ZDataset.scala
+++ src/main/scala/zio/spark/sql/ZDataset.scala
@@ -1,14 +1,15 @@
 package zio.spark.sql
-import org.apache.spark.sql.{DataFrame => UnderlyingDataFrame}
+
+import org.apache.spark.sql.{Dataset => UnderlyingDataset}
 
 import zio.Task
 
-final case class ZDataset[T](df: UnderlyingDataFrame) extends Dataset[T] {
+final case class ZDataset[T](ds: UnderlyingDataset[T]) extends Dataset[T] {
   override def limit(n: Int): Dataset[T] = transformation(_.limit(n))
 
-  def transformation(f: UnderlyingDataFrame => UnderlyingDataFrame): Dataset[T] = ZDataset(f(df))
+  def transformation(f: UnderlyingDataset[T] => UnderlyingDataset[T]): Dataset[T] = ZDataset(f(ds))
 
   override def count(): Task[Long] = action(_.count())
 
-  def action[A](f: UnderlyingDataFrame => A): Task[A] = Task.attemptBlocking(f(df))
+  def action[A](f: UnderlyingDataset[T] => A): Task[A] = Task.attemptBlocking(f(ds))
 }
