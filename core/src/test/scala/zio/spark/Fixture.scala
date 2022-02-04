70a47a91e2823e47ffccc60f591ae4d0d5f462c2
dylandoamaral <do.amaral.dylan@gmail.com>
2022-02-04 14:25:27 +0100

    fix: tests

--- core/src/test/scala/zio/spark/Fixture.scala
+++ core/src/test/scala/zio/spark/Fixture.scala
@@ -6,9 +6,9 @@ object Fixture {
   def readCsv(path: String): Spark[DataFrame] =
     SparkSession.use(_.read.inferSchema.withHeader.withDelimiter(";").csv(path))
 
-  val read: Spark[DataFrame] = readCsv("new/src/test/resources/data.csv")
+  val read: Spark[DataFrame] = readCsv("core/src/test/resources/data.csv")
 
-  val readEmpty: Spark[DataFrame] = readCsv("new/src/test/resources/empty.csv")
+  val readEmpty: Spark[DataFrame] = readCsv("core/src/test/resources/empty.csv")
 
-  val readLorem: Spark[Dataset[String]] = SparkSession.use(_.read.textFile("new/src/test/resources/lorem.txt"))
+  val readLorem: Spark[Dataset[String]] = SparkSession.use(_.read.textFile("core/src/test/resources/lorem.txt"))
 }
