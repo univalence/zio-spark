bf224b0ee4baf5eaf473eefe27767e0a2861a81f
dylandoamaral <do.amaral.dylan@gmail.com>
2022-02-08 14:25:56 +0100

    fix: compatibility test

--- core/src/test/scala/zio/spark/sql/DatasetCompatibilityTest.scala
+++ core/src/test/scala/zio/spark/sql/DatasetCompatibilityTest.scala
@@ -6,6 +6,13 @@ import zio.spark.sql.ExtraDatasetCompatibilityMethods._
 object DatasetCompatibilityTest
     extends CompatibilityTestBetween[org.apache.spark.sql.Dataset[Any], zio.spark.sql.Dataset[Any]](
       allowedNewMethods =
-        Seq("underlyingDataset", "transformation", "action", "headOption", "firstOption") ++ extraAllowedMethods,
+        Seq(
+          "underlyingDataset",
+          "transformation",
+          "action",
+          "headOption",
+          "firstOption",
+          "unpersistBlocking"
+        ) ++ extraAllowedMethods,
       isImpure = true
     )
