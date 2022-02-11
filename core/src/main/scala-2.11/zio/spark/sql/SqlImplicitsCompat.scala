d2b60675d99f0cc3ec31a5d683e32caa97bcb75e
dylandoamaral <do.amaral.dylan@gmail.com>
2022-02-11 17:15:50 +0100

    fix: import

--- core/src/main/scala-2.11/zio/spark/sql/SqlImplicitsCompat.scala
+++ core/src/main/scala-2.11/zio/spark/sql/SqlImplicitsCompat.scala
@@ -1,5 +1,7 @@
 package zio.spark.sql
 
+import org.apache.spark.sql.{ColumnName, Encoder, Encoders}
+
 import scala.reflect.runtime.universe.TypeTag
 
 trait SqlImplicits extends LowPrioritySQLImplicits {
