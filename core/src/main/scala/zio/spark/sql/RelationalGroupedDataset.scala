a2decf1ff8cde2a709248dade9f734489793c7af
Jonathan Winandy <jonathan.winandy@gmail.com>
2022-03-03 03:21:50 +0100

    refactoring: improve ergonomics

--- core/src/main/scala/zio/spark/sql/RelationalGroupedDataset.scala
+++ core/src/main/scala/zio/spark/sql/RelationalGroupedDataset.scala
@@ -13,6 +13,11 @@ object RelationalGroupedDataset {
     RelationalGroupedDataset(ImpureBox(underlyingRelationalGroupedDataset))
 }
 
+object RelationalGroupedDataset {
+  def apply(underlyingRelationalGroupedDataset: UnderlyingRelationalGroupedDataset): RelationalGroupedDataset =
+    RelationalGroupedDataset(ImpureBox(underlyingRelationalGroupedDataset))
+}
+
 final case class RelationalGroupedDataset(underlyingRelationalDataset: ImpureBox[UnderlyingRelationalGroupedDataset])
     extends Impure(underlyingRelationalDataset) {
   import underlyingRelationalDataset._
