ff851096ea40777723320507c09def5dbc5feb82
Jonathan Winandy <jonathan.winandy@gmail.com>
2022-03-14 21:33:17 +0100

    doc: update comments

--- zio-spark-core/src/main/scala/zio/spark/effect/CancellableEffect.scala
+++ zio-spark-core/src/main/scala/zio/spark/effect/CancellableEffect.scala
@@ -11,6 +11,20 @@ import scala.util.Random
 
 object CancellableEffect {
 
+  /**
+   * Make a spark job cancellable by using a unique jobGroup to send a
+   * cancel signal when ZIO is interrupting the fiber.
+   *
+   * To set the jobGroup correctly when using spark actions, the task if
+   * forced to be executed on a standard global scala execution context
+   * (scala.concurrent.ExecutionContext.Implicits.global), then call
+   * sparkContext.setJobGroup before each statement (for now, until we
+   * find a better way)
+   *
+   * @param job
+   * @tparam T
+   * @return
+   */
   @Experimental
   def makeItCancellable[T](job: Spark[T]): Spark[T] = {
     val newGroupName = UIO("cancellable-group-" + Random.alphanumeric.take(6).mkString)
@@ -27,6 +41,7 @@ object CancellableEffect {
       )
   }
 
+  //TODO : BUILD EXECUTION CONTEXT FROM THE EXISTING ZIO CONTEXT (zio.Runtime.config ...)
   private def executorContext(sparkContext: SparkContext, groupName: String): ExecutionContext =
     new ExecutionContext {
       val global: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
@@ -34,7 +49,9 @@ object CancellableEffect {
       override def execute(runnable: Runnable): Unit =
         global.execute(new Runnable {
           override def run(): Unit = {
-            sparkContext.setJobGroup(groupName, "cancellable job group")
+            if (!Option(sparkContext.getLocalProperty("spark.jobGroup.id")).contains(groupName))
+              sparkContext.setJobGroup(groupName, "cancellable job group")
+
             runnable.run()
           }
         })
