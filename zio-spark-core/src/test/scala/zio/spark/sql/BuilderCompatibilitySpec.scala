04d0bbe5ccd2ce54f6c442b44d07129221f618a4
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2022-03-30 15:32:18 +0200

    feat: implement enableHiveSupport on SparkSession (#201)

--- zio-spark-core/src/test/scala/zio/spark/sql/BuilderCompatibilitySpec.scala
+++ zio-spark-core/src/test/scala/zio/spark/sql/BuilderCompatibilitySpec.scala
@@ -4,5 +4,6 @@ import zio.spark.helper.CompatibilityTestBetween
 
 object BuilderCompatibilitySpec
     extends CompatibilityTestBetween[org.apache.spark.sql.SparkSession.Builder, zio.spark.sql.SparkSession.Builder](
-      allowedNewMethods = Seq("configs", "asLayer", "acquireRelease", "driverMemory", "builder", "extraConfigs")
+      allowedNewMethods =
+        Seq("configs", "asLayer", "acquireRelease", "driverMemory", "builder", "extraConfigs", "hiveSupport")
     )
