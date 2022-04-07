9dfa02ea61c17cb49ba379a1b6eb81d50bb6a174
Jonathan Winandy <jonathan.winandy@gmail.com>
2022-04-08 00:55:59 +0200

    refactoring: small changes
    Signed-off-by: Jonathan Winandy <jonathan.winandy@gmail.com>

--- examples/zio-ecosystem/src/main/scala/example/ZIOEcosystem.scala
+++ examples/zio-ecosystem/src/main/scala/example/ZIOEcosystem.scala
@@ -11,60 +11,45 @@ import zio.cli.HelpDoc.Span.text
 import zio.spark.parameter._
 import zio.spark.sql._
 
-import java.util.concurrent.TimeUnit
-
 object ZIOEcosystem extends ZIOAppDefault {
-  Logger.getLogger("org").setLevel(Level.OFF) // Disable Spark logging
-
   // A more sophisticated layer to add middleware logs
   private val session: ZLayer[Any, Throwable, SparkSession] =
     ZLayer.scoped {
-      for {
-        _ <- ZIO.logInfo("Opening Spark Session...")
-        builder = SparkSession.builder.master(localAllNodes).appName("zio-ecosystem")
-        session <-
-          ZIO.acquireRelease(builder.getOrCreate) { ss =>
-            for {
-              _ <- ZIO.logInfo("Closing Spark Session...")
-              _ <-
-                Task
-                  .attempt(ss.close)
-                  .foldZIO(
-                    failure = _ => ZIO.logError("Failed to close the Spark Session."),
-                    success = _ => ZIO.unit
-                  )
-            } yield ()
-          }
-      } yield session
+      val disableSparkLogging: UIO[Unit] = ZIO.succeed(Logger.getLogger("org").setLevel(Level.OFF))
+
+      val builder: SparkSession.Builder = SparkSession.builder.master(localAllNodes).appName("zio-ecosystem")
+
+      val build: ZIO[Scope, Throwable, SparkSession] =
+        builder.getOrCreate.withFinalizer { ss =>
+          ZIO.logInfo("Closing Spark Session ...") *>
+            ss.close.tapError(_ => ZIO.logError("Failed to close the Spark Session.")).orDie
+        }
+
+      ZIO.logInfo("Opening Spark Session...") *> disableSparkLogging *> build
     }
 
-  sealed trait Example { self =>
-    val name: String
-    def toArgs: (String, Example) = (name, self)
+  sealed abstract class Example {
+    def name: String = this.getClass.getName.replaceAll("(\\p{Lu}+\\p{Lo}*)", "-$1").replaceAll("^-", "").toLowerCase
   }
 
-  case object SimpleApp extends Example {
-    val name: String = "simple-app"
+  object Example {
+    case object SimpleApp extends Example
+
+    case object WordCount extends Example
+
+    case object SparkCodeMigration extends Example
+
+    case object All extends Example
+
+    val values: Seq[Example] = Seq(SimpleApp, WordCount, SparkCodeMigration, All)
+
   }
-  case object WordCount extends Example {
-    val name: String = "word-count"
-  }
-  case object SparkCodeMigration extends Example {
-    val name: String = "spark-code-migration"
-  }
-  case object All extends Example {
-    val name: String = "all"
-  }
+
+  import Example._
 
   final case class RunSubcommand(example: Example)
 
-  val runSubcommandArgs: Args[Example] =
-    Args.enumeration(
-      SimpleApp.toArgs,
-      WordCount.toArgs,
-      SparkCodeMigration.toArgs,
-      All.toArgs
-    )
+  val runSubcommandArgs: Args[Example] = Args.enumeration(Example.values.map(x => (x.name, x)): _*)
 
   val exampleSubcommand: Command[RunSubcommand] = Command("example", Options.none, runSubcommandArgs).map(RunSubcommand)
   val zioSparkCommand: Command[RunSubcommand] =
@@ -78,32 +63,44 @@ object ZIOEcosystem extends ZIOAppDefault {
       zioSparkCommand
     )(program)
 
-  def program(command: RunSubcommand): SIO[Any] =
+  def program(command: RunSubcommand): SIO[Any] = {
+
+    def logInfo(example: Example): UIO[Unit] =
+      example match {
+        case All => ZIO.logInfo("You selected 'all', running all the available examples...")
+        case _   => ZIO.logInfo(s"You selected '${example.name}', running this example...")
+      }
+
+    def job(example: Example): SIO[Unit] =
+      example match {
+        case SimpleApp          => simpleAppExample
+        case WordCount          => wordCountExample
+        case SparkCodeMigration => sparkCodeMigrationExample
+        case All                => ZIO.collectAllParDiscard(Example.values.filter(_ != All).map(job))
+      }
+
+    // operator style
+    logInfo(command.example) *> job(command.example).timed.tap { case (duration, _) =>
+      val seconds: Float = duration.toMillis.toFloat / 1000
+      ZIO.logInfo(s"Example (${command.example.name}) correctly finished, it took $seconds seconds!")
+    }
+
+    // for-comprehension style
     for {
-      _ <-
-        command.example match {
-          case All => ZIO.logInfo("You selected 'all', running all the available examples...")
-          case _   => ZIO.logInfo(s"You selected '${command.example.name}', running this example...")
-        }
-      clock  <- ZIO.clock
-      before <- clock.currentTime(TimeUnit.MICROSECONDS)
-      _ <-
-        command.example match {
-          case SimpleApp          => simpleAppExample
-          case WordCount          => wordCountExample
-          case SparkCodeMigration => sparkCodeMigrationExample
-          case All => ZIO.collectAllParDiscard(List(simpleAppExample, wordCountExample, sparkCodeMigrationExample))
-        }
-      after <- clock.currentTime(TimeUnit.MICROSECONDS)
-      duration = (after - before).toFloat / 1000000f
-      _ <- ZIO.logInfo(s"Example(s) correctly finished, it took $duration seconds!")
-    } yield ()
+      _   <- logInfo(command.example)
+      run <- job(command.example).timed
+      seconds: Float = run._1.toMillis.toFloat / 1000
+      _ <- ZIO.logInfo(s"Example (${command.example.name}) correctly finished, it took $seconds seconds!")
+    } yield {}
+  }
 
   // zio-spark run simple-app => Run the simple-app example
   // zio-spark run all        => Run all examples in parallel
-  override def run: ZIO[ZIOAppArgs with Scope, Nothing, Unit] =
+  override def run: URIO[ZIOAppArgs with Scope, Unit] =
     for {
-      // args <- ZIOAppArgs.getArgs // We comment this line for the example, we don't use arguments directly.
-      _ <- app.run(List("example", "all")).provide(session).orDie
+      args <- ZIOAppArgs.getArgs
+      _    <- app.run(List("example", "all")).provide(session).orDie
+      // We comment this line for the example, we don't use arguments directly.
+      // _    <- app.run(args.toList).provide(session).orDie
     } yield ()
 }
