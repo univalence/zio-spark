d39adb1664745932f7294a9dd2ffc2b325a70506
dylandoamaral <do.amaral.dylan@gmail.com>
2022-12-06 15:18:49 +0100

    chore: remove useless import

--- zio-spark-codegen/src/main/scala/zio/spark/codegen/generation/template/instance/DatasetTemplate.scala
+++ zio-spark-codegen/src/main/scala/zio/spark/codegen/generation/template/instance/DatasetTemplate.scala
@@ -43,13 +43,11 @@ case object DatasetTemplate extends Template.Default {
         case ScalaBinaryVersion.V2_13 | ScalaBinaryVersion.V3 =>
           s"""$baseImports
              |import org.apache.spark.sql.execution.ExplainMode
-             |import scala.jdk.CollectionConverters._
-             |import org.apache.spark.sql.types.Metadata""".stripMargin
+             |import scala.jdk.CollectionConverters._""".stripMargin
         case ScalaBinaryVersion.V2_12 =>
           s"""$baseImports
              |import org.apache.spark.sql.execution.ExplainMode
-             |import scala.collection.JavaConverters._
-             |import org.apache.spark.sql.types.Metadata""".stripMargin
+             |import scala.collection.JavaConverters._""".stripMargin
         case ScalaBinaryVersion.V2_11 =>
           s"""$baseImports
              |import org.apache.spark.sql.execution.command.ExplainCommand
