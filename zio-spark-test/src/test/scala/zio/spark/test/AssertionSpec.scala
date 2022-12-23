79f74ef1536212ca823d52ad6bd56a17a2c3291e
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2022-12-23 11:41:53 +0100

    chore: add zio spark test in ci (#295)

--- zio-spark-test/src/test/scala/zio/spark/test/AssertionSpec.scala
+++ zio-spark-test/src/test/scala/zio/spark/test/AssertionSpec.scala
@@ -1,3 +1,13 @@
 package zio.spark.test
 
-object AssertionSpec {}
+import zio._
+import zio.test._
+
+object AssertionSpec extends ZIOSpecDefault {
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    suite("temp spec")(
+      test("temp test") {
+        assertTrue(1 == 1)
+      }
+    )
+}
