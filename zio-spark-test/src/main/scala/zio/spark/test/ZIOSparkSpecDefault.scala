9353d29dacb9772925f5da7606a56013bc156c34
dylandoamaral <do.amaral.dylan@gmail.com>
2022-12-27 10:26:10 +0100

    feat: add spec default

---
+++ zio-spark-test/src/main/scala/zio/spark/test/ZIOSparkSpecDefault.scala
@@ -0,0 +1,17 @@
+package zio.spark.test
+
+import org.apache.log4j._
+
+import zio._
+import zio.spark.sql._
+import zio.test._
+
+abstract class ZIOSparkSpecDefault extends ZIOSpecDefault {
+  Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
+  Logger.getLogger("org.apache.hadoop").setLevel(Level.OFF)
+
+  def sparkSpec: Spec[SparkSession with TestEnvironment with Scope, Any]
+
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    sparkSpec.provideSomeLayer[TestEnvironment with Scope](defaultSparkSession.asLayer)
+}
---
+++ zio-spark-test/src/test/scala/zio/spark/test/ZIOSparkSpecDefaultSpec.scala
@@ -0,0 +1,16 @@
+package zio.spark.test
+
+import zio.spark.sql.implicits._
+import zio.test._
+
+object ZIOSparkSpecDefaultSpec extends ZIOSparkSpecDefault {
+  override def sparkSpec =
+    suite("ZIOSparkSpecDefault can run spark job without providing layer")(
+      test("It can run DataFrame job") {
+        for {
+          df    <- Dataset(1, 2, 3)
+          count <- df.count
+        } yield assertTrue(count == 3)
+      }
+    )
+}
