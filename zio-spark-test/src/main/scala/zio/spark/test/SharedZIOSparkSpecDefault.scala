a92b362c0787e2a5819a4a073728502d69b419dc
dylandoamaral <do.amaral.dylan@gmail.com>
2022-12-28 09:28:42 +0100

    refactor: allow spec default customisation

--- zio-spark-test/src/main/scala/zio/spark/test/SharedZIOSparkSpecDefault.scala
+++ zio-spark-test/src/main/scala/zio/spark/test/SharedZIOSparkSpecDefault.scala
@@ -10,6 +10,8 @@ abstract class SharedZIOSparkSpecDefault extends ZIOSpec[SparkSession] {
   Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
   Logger.getLogger("org.apache.hadoop").setLevel(Level.OFF)
 
+  def ss = defaultSparkSession
+
   @SuppressWarnings(Array("scalafix:DisableSyntax.valInAbstract"))
-  override val bootstrap: TaskLayer[SparkSession] = defaultSparkSession.asLayer
+  override val bootstrap: TaskLayer[SparkSession] = ss.asLayer
 }
