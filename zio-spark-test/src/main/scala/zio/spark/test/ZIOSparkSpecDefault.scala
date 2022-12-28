d242a6cf6e6dab80968965a69dca29e648dea2c3
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2022-12-28 09:55:43 +0100

    refactor: allow spec default customisation (#302)

--- zio-spark-test/src/main/scala/zio/spark/test/ZIOSparkSpecDefault.scala
+++ zio-spark-test/src/main/scala/zio/spark/test/ZIOSparkSpecDefault.scala
@@ -10,8 +10,10 @@ abstract class ZIOSparkSpecDefault extends ZIOSpecDefault {
   Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
   Logger.getLogger("org.apache.hadoop").setLevel(Level.OFF)
 
+  def ss = defaultSparkSession
+
   def sparkSpec: Spec[SparkSession with TestEnvironment with Scope, Any]
 
   override def spec: Spec[TestEnvironment with Scope, Any] =
-    sparkSpec.provideSomeLayer[TestEnvironment with Scope](defaultSparkSession.asLayer)
+    sparkSpec.provideSomeLayer[TestEnvironment with Scope](ss.asLayer)
 }
