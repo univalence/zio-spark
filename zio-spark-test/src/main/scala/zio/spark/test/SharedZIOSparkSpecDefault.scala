ff8528fc47995f60c6dbad5e880e0828831d0542
dylandoamaral <do.amaral.dylan@gmail.com>
2023-01-02 11:56:14 +0100

    fix: style

--- zio-spark-test/src/main/scala/zio/spark/test/SharedZIOSparkSpecDefault.scala
+++ zio-spark-test/src/main/scala/zio/spark/test/SharedZIOSparkSpecDefault.scala
@@ -1,20 +1,14 @@
 package zio.spark.test
 
-import org.apache.log4j._
-
 import zio._
 import zio.spark.sql._
 import zio.test._
 
 abstract class SharedZIOSparkSpecDefault extends ZIOSpec[SparkSession] {
-  Logger.getLogger("org").setLevel(Level.OFF)
-  Logger.getLogger("akka").setLevel(Level.OFF)
-
   def ss = defaultSparkSession
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.valInAbstract"))
   override val bootstrap: TaskLayer[SparkSession] =
-    ss
-    .asLayer
-    .tap(_.get.sparkContext.setLogLevel("ERROR"))
+    ss.asLayer
+      .tap(_.get.sparkContext.setLogLevel("ERROR"))
 }
--- zio-spark-test/src/main/scala/zio/spark/test/ZIOSparkSpecDefault.scala
+++ zio-spark-test/src/main/scala/zio/spark/test/ZIOSparkSpecDefault.scala
@@ -1,23 +1,17 @@
 package zio.spark.test
 
-import org.apache.log4j._
-
 import zio._
 import zio.spark.sql._
 import zio.test._
 
 abstract class ZIOSparkSpecDefault extends ZIOSpecDefault {
-  Logger.getLogger("org").setLevel(Level.OFF)
-  Logger.getLogger("akka").setLevel(Level.OFF)
-
   def ss = defaultSparkSession
 
   def sparkSpec: Spec[SparkSession with TestEnvironment with Scope, Any]
 
   override def spec: Spec[TestEnvironment with Scope, Any] =
     sparkSpec.provideSomeLayer[TestEnvironment with Scope](
-      ss
-        .asLayer
+      ss.asLayer
         .tap(_.get.sparkContext.setLogLevel("ERROR"))
     )
 }
