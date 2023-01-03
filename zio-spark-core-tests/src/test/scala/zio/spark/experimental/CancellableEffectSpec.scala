3847993eafc3add05f62ad6db36647de9a2f08ef
dylandoamaral <do.amaral.dylan@gmail.com>
2023-01-03 14:24:15 +0100

    fix: style

--- zio-spark-core-tests/src/test/scala/zio/spark/experimental/CancellableEffectSpec.scala
+++ zio-spark-core-tests/src/test/scala/zio/spark/experimental/CancellableEffectSpec.scala
@@ -3,9 +3,10 @@ package zio.spark.experimental
 import org.apache.spark.SparkContextCompatibility.removeSparkListener
 import org.apache.spark.SparkFirehoseListener
 import org.apache.spark.scheduler.{SparkListenerEvent, SparkListenerJobEnd, SparkListenerJobStart}
-import scala3encoders.given
-import zio.{Chunk, Ref, UIO, Unsafe, ZIO, durationInt, durationLong}
-import zio.spark.sql.{SIO, SparkSession, fromSpark}
+import scala3encoders.given // scalafix:ok
+
+import zio.{durationInt, durationLong, Chunk, Ref, UIO, Unsafe, ZIO}
+import zio.spark.sql.{fromSpark, SIO, SparkSession}
 import zio.spark.sql.implicits._
 import zio.spark.test._
 import zio.test._
