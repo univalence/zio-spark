9d0d7b179c21259b3e23183a480b2f443b58a925
dylandoamaral <do.amaral.dylan@gmail.com>
2023-01-13 10:02:39 +0100

    feat: add more type conversion

--- zio-spark-test/src/main/scala/zio/spark/test/internal/ToPositionalValueMatcher.scala
+++ zio-spark-test/src/main/scala/zio/spark/test/internal/ToPositionalValueMatcher.scala
@@ -3,12 +3,29 @@ package zio.spark.test.internal
 
 import zio.spark.test.internal.ValueMatcher.PositionalValueMatcher._
 import zio.spark.test.internal.ValueMatcher.PositionalValueMatcher
+
+import java.time.{LocalDate, LocalDateTime, OffsetDateTime}
 trait ToPositionalValueMatcher[T] {
   def apply(t: T): PositionalValueMatcher
 }
 
 object ToPositionalValueMatcher {
-  implicit val int: ToPositionalValueMatcher[Int] = t => Value(t)
-  implicit val string: ToPositionalValueMatcher[String] = t => Value(t)
-  implicit def predicate[T]: ToPositionalValueMatcher[T => Boolean] = f => Predicate(f)
+  implicit def array[T]: ToPositionalValueMatcher[Seq[T]] = Value.apply
+  implicit def map[K, V]: ToPositionalValueMatcher[Map[K, V]] = Value.apply
+
+  implicit val boolean: ToPositionalValueMatcher[Boolean] = Value.apply
+  implicit val byte: ToPositionalValueMatcher[Byte] = Value.apply
+  implicit val char: ToPositionalValueMatcher[Char] = Value.apply
+  implicit val datetimeLocal: ToPositionalValueMatcher[LocalDateTime] = Value.apply
+  implicit val datetimeOffset: ToPositionalValueMatcher[OffsetDateTime] = Value.apply
+  implicit val date: ToPositionalValueMatcher[LocalDate] = Value.apply
+  implicit val decimal: ToPositionalValueMatcher[BigDecimal] = Value.apply
+  implicit val double: ToPositionalValueMatcher[Double] = Value.apply
+  implicit val float: ToPositionalValueMatcher[Float] = Value.apply
+  implicit val int: ToPositionalValueMatcher[Int] = Value.apply
+  implicit val long: ToPositionalValueMatcher[Long] = Value.apply
+  implicit val short: ToPositionalValueMatcher[Short] = Value.apply
+  implicit val string: ToPositionalValueMatcher[String] = Value.apply
+
+  implicit def predicate[T]: ToPositionalValueMatcher[T => Boolean] = Predicate.apply
 }
