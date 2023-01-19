8e0112c0d882be5331bde3665d55c63a09ddfb4d
dylandoamaral <do.amaral.dylan@gmail.com>
2023-01-19 17:14:30 +0100

    feat: add create dataset

--- zio-spark-core/src/main/scala/zio/spark/sql/SparkSession.scala
+++ zio-spark-core/src/main/scala/zio/spark/sql/SparkSession.scala
@@ -1,14 +1,18 @@
 package zio.spark.sql
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{Encoder, SparkSession => UnderlyingSparkSession}
+import org.apache.spark.sql.{Encoder, Row, SparkSession => UnderlyingSparkSession}
+import org.apache.spark.sql.types.StructType
 
 import zio._
 import zio.spark.SparkContext
 import zio.spark.parameter._
+import zio.spark.rdd.RDD
 import zio.spark.sql.DataFrameReader.WithoutSchema
 import zio.spark.sql.SparkSession.Conf
 import zio.spark.sql.streaming.DataStreamReader
+
+import scala.reflect.runtime.universe.TypeTag
 
 final case class SparkSession(underlyingSparkSession: UnderlyingSparkSession)
     extends ExtraSparkSessionFeature(underlyingSparkSession) {
@@ -24,6 +28,56 @@ final case class SparkSession(underlyingSparkSession: UnderlyingSparkSession)
 
   /** Creates a new [[Dataset]] of type T containing zero elements. */
   def emptyDataset[T: Encoder]: Dataset[T] = Dataset(underlyingSparkSession.emptyDataset[T])
+
+  /**
+   * Creates a `DataFrame` from an RDD of Product (e.g. case classes,
+   * tuples).
+   *
+   * @since 2.0.0
+   */
+  def createDataFrame[A <: Product: TypeTag](rdd: RDD[A]): Task[DataFrame] =
+    ZIO.attempt(Dataset(underlyingSparkSession.createDataFrame(rdd.underlying)))
+
+  /**
+   * Creates a `DataFrame` from a local Seq of Product.
+   *
+   * @since 2.0.0
+   */
+  def createDataFrame[A <: Product: TypeTag](data: Seq[A]): Task[DataFrame] =
+    ZIO.attempt(Dataset(underlyingSparkSession.createDataFrame(data)))
+
+  /**
+   * Creates a `DataFrame` from an `RDD` containing [[Row]]s using the
+   * given schema. It is important to make sure that the structure of
+   * every [[Row]] of the provided RDD matches the provided schema.
+   * Otherwise, there will be runtime exception. Example:
+   * {{{
+   *   import org.apache.spark.sql._
+   *   import org.apache.spark.sql.types._
+   *   val sparkSession = new org.apache.spark.sql.SparkSession(sc)
+   *
+   *   val schema =
+   *     StructType(
+   *       StructField("name", StringType, false) ::
+   *       StructField("age", IntegerType, true) :: Nil)
+   *
+   *   val people =
+   *     sc.textFile("examples/src/main/resources/people.txt").map(
+   *       _.split(",")).map(p => Row(p(0), p(1).trim.toInt))
+   *   val dataFrame = sparkSession.createDataFrame(people, schema)
+   *   dataFrame.printSchema
+   *   // root
+   *   // |-- name: string (nullable = false)
+   *   // |-- age: integer (nullable = true)
+   *
+   *   dataFrame.createOrReplaceTempView("people")
+   *   sparkSession.sql("select name from people").collect.foreach(println)
+   * }}}
+   *
+   * @since 2.0.0
+   */
+  def createDataFrame(rowRDD: RDD[Row], schema: StructType): Task[DataFrame] =
+    ZIO.attempt(Dataset(underlyingSparkSession.createDataFrame(rowRDD.underlying, schema)))
 
   def conf: Conf =
     new Conf {
@@ -46,6 +100,56 @@ object SparkSession {
 
   /** Creates a new [[Dataset]] of type T containing zero elements. */
   def emptyDataset[T: Encoder]: RIO[SparkSession, Dataset[T]] = ZIO.service[SparkSession].map(_.emptyDataset[T])
+
+  /**
+   * Creates a `DataFrame` from an RDD of Product (e.g. case classes,
+   * tuples).
+   *
+   * @since 2.0.0
+   */
+  def createDataFrame[A <: Product: TypeTag](rdd: RDD[A]): RIO[SparkSession, DataFrame] =
+    ZIO.service[SparkSession].flatMap(_.createDataFrame(rdd))
+
+  /**
+   * Creates a `DataFrame` from a local Seq of Product.
+   *
+   * @since 2.0.0
+   */
+  def createDataFrame[A <: Product: TypeTag](data: Seq[A]): RIO[SparkSession, DataFrame] =
+    ZIO.service[SparkSession].flatMap(_.createDataFrame(data))
+
+  /**
+   * Creates a `DataFrame` from an `RDD` containing [[Row]]s using the
+   * given schema. It is important to make sure that the structure of
+   * every [[Row]] of the provided RDD matches the provided schema.
+   * Otherwise, there will be runtime exception. Example:
+   * {{{
+   *   import org.apache.spark.sql._
+   *   import org.apache.spark.sql.types._
+   *   val sparkSession = new org.apache.spark.sql.SparkSession(sc)
+   *
+   *   val schema =
+   *     StructType(
+   *       StructField("name", StringType, false) ::
+   *       StructField("age", IntegerType, true) :: Nil)
+   *
+   *   val people =
+   *     sc.textFile("examples/src/main/resources/people.txt").map(
+   *       _.split(",")).map(p => Row(p(0), p(1).trim.toInt))
+   *   val dataFrame = sparkSession.createDataFrame(people, schema)
+   *   dataFrame.printSchema
+   *   // root
+   *   // |-- name: string (nullable = false)
+   *   // |-- age: integer (nullable = true)
+   *
+   *   dataFrame.createOrReplaceTempView("people")
+   *   sparkSession.sql("select name from people").collect.foreach(println)
+   * }}}
+   *
+   * @since 2.0.0
+   */
+  def createDataFrame(rowRDD: RDD[Row], schema: StructType): RIO[SparkSession, DataFrame] =
+    ZIO.service[SparkSession].flatMap(_.createDataFrame(rowRDD, schema))
 
   def conf: URIO[SparkSession, Conf] = ZIO.service[SparkSession].map(_.conf)
 
